24dd7b937972d5d405f305a0ab10038770c9d88a
SOFTWARE\achroscielewska <agnieszka.chroscielewska@altkom.pl>
2019-07-13 19:14:47 +0200

    delete rooute added

--- booksehlf-api/src/app/routes/books.js
+++ booksehlf-api/src/app/routes/books.js
@@ -3,28 +3,48 @@ const router = express.Router();
 
 // initial data
 const books = [
-    {id: 'acb1', title: 'title', description: 'content', bookshelfNo: '1a' },
-    {id: 'acb2', title: 'title2', description: 'content2', bookshelfNo: '1a'},
-]
+  { id: 123, title: "title", description: "content", bookshelfNo: "1a" },
+  { id: 124, title: "title2", description: "content2", bookshelfNo: "1a" }
+];
 
-const index = 2;
+// log request time
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+// remove book
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  const index = books.findIndex(book => book.id == "123");
+
+  const updatedBooks = [
+    ...books.slice(0, index),
+    ...books.slice(index + 1)
+  ];
+
+  console.log(updatedBooks);
+
+  res.status(201).json({ message: "Book deleted successfully" });
+});
 
 // add new book
-router.post('/newBook', (req, res, next) => {
-    const book = req.body;
-    book.id = 'acb';
+router.post("/newBook", (req, res, next) => {
+  const book = req.body;
+  book.id = "acb";
 
-    books.push(book)
-    console.log(books);
-    res.status(201).json({ message: 'Book added successfully' })
-})
+  books.push(book);
+  console.log(books);
+  res.status(201).json({ message: "Book added successfully" });
+});
 
 // get all books
-router.get('',(req, res, next) => {
-    res.status(200).json({
-        message: 'Books send',
-        books: books
-    });
-})
+router.get("", (req, res, next) => {
+  res.status(200).json({
+    message: "Books send",
+    books: books
+  });
+});
 
 module.exports = router;
